d9e81e869e319f4276bc0f72575d24e6bcd555fa
mikhailanufriev <mikhail.anufriev.post@gmail.com>
2021-02-16 11:30:45 +0300

    weather

--- weather.py
+++ weather.py
@@ -38,7 +38,7 @@ def get_text_messages(message):
     if message.text.lower() == 'погода':
         bot.send_message(message.from_user.id, 'погода сегодня супер, на улице {0}'.format(data['weather'][0]['description']))
         bot.send_message(message.from_user.id, 'температура сейчас, {0}'.format(data['main']['temp']))
-        if data['main']['temp']<0:
+        if int(data['main']['temp'])<0:
            bot.send_message(message.from_user.id, 'зима')
     elif message.text.lower() == 'прогноз':
         bot.send_message(message.from_user.id, 'сообщаю прогноз погоды ')
